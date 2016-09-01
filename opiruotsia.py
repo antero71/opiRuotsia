33951cd02660268d07b5b5471d8d08305b2aff8c
Antero Oikkonen <antero.oikkonen@gmail.com>
2016-09-01 15:15:41 +0300

    Ruotsin opiskelia, epäsäännölliset verbit

---
+++ opiruotsia.py
@@ -0,0 +1,37 @@
+from random import randint
+def readFile(tiedostonNimi):
+    file = open(tiedostonNimi,"r")
+    lines = file.readlines()
+    file.close()
+    return lines
+
+def tarkistaVastaus(kieli,vastaus):
+    if kieli=="r" and vastaus==outer_list[index][index_ruotsi]:
+        print("oikein!")
+    elif kieli=="s" and vastaus==outer_list[index][index_suomi]:
+        print("Oikein!")
+    elif kieli=="r":
+        print("Väärin")
+        print("Oikea vastaus on "+str(outer_list[index][index_ruotsi]))
+    else:
+        print("Väärin")
+        print("Oikea vastaus on "+str(outer_list[index][index_suomi]))
+
+
+
+outer_list=[]
+lines = readFile("sanasto.txt")
+for x in range(0,len(lines)):
+    outer_list.append(lines[x].strip().split(";"))
+
+index_ruotsi=0
+index_suomi=1
+kieli=input("vastaa kielellä, ruotsi=r ja suomi=s ")
+index=randint(0,100)
+if kieli=="r":
+    print("suomi "+str(outer_list[index][index_suomi]))
+else:
+    print("ruotsi "+str(outer_list[index][index_ruotsi]))
+vastaus=input("Anna sana toisella kielellä ")
+tarkistaVastaus(kieli,vastaus)
+
