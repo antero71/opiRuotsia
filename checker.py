00c99606b391bf3d215ba1b0ed8554e77587e46f
Antero Oikkonen <antero.oikkonen@gmail.com>
2017-08-10 16:49:43 +0300

    I added a new class Checker

---
+++ checker.py
@@ -0,0 +1,22 @@
+class Checker():
+    def __init__(self, right_answer):
+        self.right_answer=right_answer
+
+
+    def check(self,answer):
+        print(len(self.right_answer))
+        points = 0
+        for a in self.right_answer:
+            if answer.__contains__(a) and len(self.right_answer)==4:
+                points += 1
+            elif answer.__contains__(a) and len(self.right_answer)==2:
+                points += 2
+            elif answer.__contains__(a) and len(self.right_answer)==1:
+                points += 4
+            elif answer.__contains__(a) and len(self.right_answer)==3:
+                points += 4
+        #print("points ", points)
+        if points > 0 and points < 4 and len(self.right_answer)<4:
+            points=4
+        print("You get ", points," points")
+        return points
--- learn_language.py
+++ learn_language.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 from random import randint
 from readfile import readFile
+from checker import Checker
 
 
 from readfile import readFile
+
+
 def check_answer(language, answer, points):
-    if language== "r" and check(answer,outer_list[index][index_sverige]):
-        print("oikein!")
-        points+=1
-    elif language== "s" and answer==outer_list[index][index_finnish]:
-        print("Oikein!")
-        points+=1
-    elif language== "r":
-        print("Väärin")
-        print("Oikea vastaus on " + str(outer_list[index][index_sverige]))
-    else:
-        print("Väärin")
-        print("Oikea vastaus on " + str(outer_list[index][index_finnish]))
+    if language== "r":
+        p=Checker((outer_list[index][index_sverige]).split(",")).check(answer)
+        if p > 0 and p < 4:
+            print("Osa oikein!")
+            print("Oikea vastaus on " + str(outer_list[index][index_sverige]))
+        elif p == 4:
+            print("Kaikki oikein!!!")
+        elif p == 0:
+            print("Ei yhtään oikein")
+            print("Oikea vastaus on " + str(outer_list[index][index_sverige]))
+        points+=p;
+    elif language== "s":
+        p = Checker((outer_list[index][index_finnish]).split(",")).check(answer)
+        if p > 0 and p < 4:
+            print("Osa oikein!")
+            print("Oikea vastaus on " + str(outer_list[index][index_finnish]))
+        elif p == 4:
+            print("Kaikki oikein!!!")
+        elif p == 0:
+            print("Ei yhtään oikein")
+            print("Oikea vastaus on " + str(outer_list[index][index_finnish]))
+        points += p;
     return points
-
-def check(answer,right_answer):
-    right = right_answer.split(",")
-    points = 0
-    for a in right:
-        if a.__contains__("answer"):
-            points+=1
-    print("points ",points)
-    return len(answer)>0 and right_answer.__contains__(answer)
-
-
-
 
 outer_list=[]
 lines = readFile("sanasto.txt")
@@ -42,18 +43,20 @@ index_sverige=0
 index_finnish=1
 language=input("vastaa kielellä, ruotsi=r ja suomi=s ")
 while 1==1:
-   count+=1
-   index=randint(0,100)
-   if language== "r":
-       print("suomi " + str(outer_list[index][index_finnish]))
-   else:
-       print("ruotsi " + str(outer_list[index][index_sverige]))
-   answer=input("Anna sana toisella kielellä\n")
-   points=check_answer(language, answer, points)
-   resume=input("Jatketaanko k/e ")
-   if resume== "e":
-      print("Kiitos ruotsin opiskelusta, tervetuloa uudelleen!")
-      prosentit = points / count * 100
-      print("sait " + str(points) + "/" + str(count) + " pistetta, eli %22.2f prosenttia oikein" % prosentit)
-      break;
+    count+=4
+    index=randint(0,100)
+    if language== "r":
+        print("suomi " + str(outer_list[index][index_finnish]))
+    else:
+        print("ruotsi " + str(outer_list[index][index_sverige]))
+    answer=input("Anna sana toisella kielellä\n")
+    points=check_answer(language, answer, points)
+    #check_answer(language, answer, points)
+
+    resume=input("Jatketaanko k/e ")
+    if resume== "e":
+        print("Kiitos ruotsin opiskelusta, tervetuloa uudelleen!")
+        percent = points / count * 100
+        print("sait " + str(points) + "/" + str(count) + " pistetta, eli %22.2f prosenttia oikein" % percent)
+        break;
 
